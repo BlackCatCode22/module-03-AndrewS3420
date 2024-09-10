858105e3f7a3c0ee3add11233be59055d9e19572
AndrewS3420 <andrewsanchez1123@gmail.com>
2024-09-10 11:53:23 -0700

    Update App.java

--- App.java
+++ App.java
@@ -1,21 +1,44 @@
-package mystudent;
-
+// AS 9/10/24
+// App.java
+// String point to create objects from my class
 public class App {
     public static void main(String[] args) {
-        System.out.println("\n Hello from class App \n\n");
 
-        // Create a student object
-        Student myStudent = new Student();
 
-        // Fill the new myStudent object's data fields
-        myStudent.firstName = "Dennis";
-        myStudent.lastname = "Mohle";
-        myStudent.gpa = 3.4;
-        myStudent.major = "Computer Science";
-        myStudent.city = "Fresno";
+        System.out.println("\n\nWelome to classes and object- oriented programming\n");
 
-        System.out.println("\n The first name of the student is: " + myStudent.firstName + "\n");
-        System.out.println("\n The last name of the student is: " + myStudent.lastname + "\n");
+        Student myNewStudent = new Student();
+
+        myNewStudent.age = 66;
+        myNewStudent.GPA = 3.4;
+        myNewStudent.major = "History";
+        myNewStudent.firstName = "Bob";
+        myNewStudent.lastname = "Marley";
+        myNewStudent.onProbation = true;
+
+        // Output some stuff about our new object
+        System.out.println("\n the first name of our student is: " + myNewStudent.firstName + "\n\n");
+
+        // Create two more students
+        Student anotherStudent = new Student();
+        Student oneMoreStu = new Student();
+
+         oneMoreStu.major = "history";
+
+         anotherStudent.age = 25;
+
+        // How many students
+        System.out.println("\n there were " + Student.getNumOfStudents() + "students objects created");
+
+        //Create a phone object
+        Phone myNewPhone = new Phone();
+
+        myNewPhone.model = "Iphone 15";
+        myNewPhone.numOfCameras = 3;
+
+        System.out.println("\n myNewPhone has " + myNewPhone.numOfCameras + " cameras\n");
+
+
 
     }
 }
